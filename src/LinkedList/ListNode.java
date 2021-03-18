19a2d7b78a31106c48a29f07f0999b71835c3e59
11trees <55312757+11trees@users.noreply.github.com>
2021-03-18 13:14:37 +0800

    Add function toListNode(int[])

--- src/LinkedList/ListNode.java
+++ src/LinkedList/ListNode.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.List;
+
 public class ListNode {
     int val;
     ListNode next;
@@ -11,5 +13,17 @@ public class ListNode {
     public String toString() {
         if(this.next == null) return this.val + " ";
         return this.val + " " + this.next.toString();
+    }
+
+    public static ListNode toListNode(int[] arrays){
+        ListNode head = new ListNode();
+        ListNode p = head;
+        for(int i = 0 ; i < arrays.length ; i++){
+            ListNode node = new ListNode(arrays[i],null);
+            p.next = node;
+            //step
+            p = p.next;
+        }
+        return head.next;
     }
 }
