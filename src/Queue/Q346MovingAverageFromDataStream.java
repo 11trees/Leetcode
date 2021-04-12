e360a8beb9b957bb43564e28bac1321b1f5cc0b3
11trees <55312757+11trees@users.noreply.github.com>
2021-04-12 10:48:18 +0800

    Q346

---
+++ src/Queue/Q346MovingAverageFromDataStream.java
@@ -0,0 +1,35 @@
+package Queue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Q346MovingAverageFromDataStream {
+    private static class MovingAverage {
+        final int MAXSIZE;
+        int currentSize = 0;
+        int sum;
+        Deque<Integer> deque = new LinkedList<>();
+
+        /** Initialize your data structure here. */
+        public MovingAverage(int size) {
+            MAXSIZE = size;
+        }
+
+        public double next(int val) {
+            if( currentSize == MAXSIZE){
+                sum -= deque.removeFirst();
+                deque.addLast(val);
+                sum += val;
+            }
+
+            if( currentSize < MAXSIZE ){
+                deque.addLast(val);
+                sum += val;
+                currentSize++;
+            }
+
+            return sum * 1.0 / currentSize;
+        }
+    }
+
+}
